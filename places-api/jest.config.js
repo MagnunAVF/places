d426657ab8bbda0920fbb994e95944c5b0975159
MagnunAVF <magnunavf@gmail.com>
2025-02-08 23:51:37 -0300

    chore(places-api): add jest test coverage configs

--- places-api/jest.config.js
+++ places-api/jest.config.js
@@ -2,4 +2,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['dist', 'node_modules'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 }
