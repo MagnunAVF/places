53be0934ce927aedf8ba9d6b353ba8b66d5c8c57
MagnunAVF <magnunavf@gmail.com>
2025-11-09 03:34:45 -0300

    feat(client): create Home Page

--- places-client/components/Message.tsx
+++ places-client/components/Message.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import {
-  AiOutlineDiscord,
-  AiTwotoneBug,
-  AiTwotoneExperiment,
-  AiOutlineTaobao,
-  AiOutlineSpotify,
-  AiOutlineCode,
-  AiOutlineCiCircle,
-} from "react-icons/ai";
+import { BiWorld } from "react-icons/bi";
+import { FaSearch } from "react-icons/fa";
+import { MdPlace } from "react-icons/md";
 
-const icons = [
-  { Icon: AiTwotoneBug },
-  { Icon: AiOutlineDiscord },
-  { Icon: AiTwotoneExperiment },
-  { Icon: AiOutlineTaobao },
-  { Icon: AiOutlineSpotify },
-  { Icon: AiOutlineCode },
-  { Icon: AiOutlineCiCircle },
-];
+const icons = [{ Icon: BiWorld }, { Icon: FaSearch }, { Icon: MdPlace }];
 
 const Customers: React.FC = () => {
   return (
@@ -26,11 +12,14 @@ const Customers: React.FC = () => {
         <div className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto">
           <div className="flex flex-col md:flex-row w-full">
             <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-6 md:mb-0 text-white dark:text-black text-center md:text-left md:w-1/3">
-              Over 50,000 people rely on our app for their daily needs
+              Thousands of places around the world for you to enjoy.
             </h2>
             <div className="flex flex-wrap justify-center md:justify-end gap-6 md:w-2/3">
               {icons.map(({ Icon }, index) => (
-                <div key={index} className="flex flex-col items-center justify-center">
+                <div
+                  key={index}
+                  className="flex flex-col items-center justify-center"
+                >
                   <Icon className="text-3xl md:text-4xl text-white dark:text-black" />
                 </div>
               ))}
